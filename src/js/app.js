36be94b1c76c207c6de7ca2bfbbdcaebda31ab3f
max.iw15 <max.iw15@yandex.ru>
2023-09-27 14:40:26 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,5 @@ export default function getRandomNumber(prev_count) {
   let temp = Math.floor(Math.random() * (max - min + 1)) + min;
   if (temp === prev_count) {
     return temp % 2;
-    //temp = getRandomNumber(prev_count)
   } else return temp;
 }
