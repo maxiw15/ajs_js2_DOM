1f5e433cfcaa7af2aef4b923a8227d403162bda5
max.iw15 <max.iw15@yandex.ru>
2023-09-29 16:08:43 +0300

    fix

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,13 +1,14 @@
 import getRandomNumber from "./GetRandomNumber";
 import expect from "expect";
 
-describe("getRandomNumber", () => {test("should return a number between 0 and 15", () => {
+describe("getRandomNumber", () => {
+  test("should return a number between 0 and 15", () => {
     const randomNumber = getRandomNumber(0);
     expect(randomNumber).toBeGreaterThanOrEqual(0);
     expect(randomNumber).toBeLessThanOrEqual(15);
   });
 
-it("should not return the same number as previous count", () => {
+  it("should not return the same number as previous count", () => {
     const prev_count = 5;
     const randomNumber = getRandomNumber(prev_count);
     expect(randomNumber).not.toBe(prev_count);
